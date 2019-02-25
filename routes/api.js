bb12201225362e65ab74b8ee769d37f9e44b88db
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-02-26 04:25:19 +0900

    api 출력 정리

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var bodyparser = require('mysql');
 var passport = require('passport');
 var router = express.Router();
 
@@ -10,18 +11,21 @@ router.get('/', function(req, res, next) {
 
 /* login related functions. */
 router.post('/login', function(req, res, next) {
-  res.status(500)
+  res.status(200)
+  // pid, 자동 로그인 토큰 전송
   res.send('respond with a resource');
 });
 
 router.post('/register', function(req, res, next) {
-  res.status(500)
+  res.status(200)
+  // 정상 작동 여부 전송
   res.send('respond with a resource');
 });
 
 router.post('/logout', function(req, res, next) {
-  // 자동 로그인 삭제 필요
-  res.status(500)
+  // 자동 로그인 토큰 삭제 필요
+  res.status(200)
+  // 1
   res.send('respond with a resource');
 });
 
@@ -29,6 +33,7 @@ router.post('/logout', function(req, res, next) {
 router.post('/get/:data', function(req, res, next) {
   // username, profile img
   res.status(500)
+  // 받아온 데이터
   res.send('respond with a resource');
 });
 
@@ -36,12 +41,14 @@ router.post('/get/:data', function(req, res, next) {
 router.post('/create/:data/', function(req, res, next) {
   // auth key, auto-login key
   res.status(200)
+  // 정상 작동 여부 전송
   res.send('respond with a resource');
 });
 
 router.post('/verify/:data', function(req, res, next) {
   // password, auth key, auto-login key
   res.status(200)
+  // 0, 1
   res.send('respond with a resource');
 });
 
