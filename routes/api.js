cd3467f6ed725cc8eeba4122391f4206e86c2a5e
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-03-02 22:50:33 +0900

    POST Data Prototype

--- routes/api.js
+++ routes/api.js
@@ -11,18 +11,48 @@ router.get('/', function(req, res, next) {
 
 /* login related functions. */
 router.post('/login', function(req, res, next) {
+
+  var input = {
+    type: 'login',
+    clientid: 1234,
+
+    userid: 'userid',
+    password: 'hashed?',
+
+    isAuth: true,
+    isAuthOn: false,
+    isWeb: true
+  }
+
   res.status(200)
   // pid, 자동 로그인 토큰 전송
   res.send('respond with a resource');
 });
 
 router.post('/register', function(req, res, next) {
+  var input = {
+    type: 'register',
+    clientid: 1234,
+
+    userid: 'userid',
+    password: 'hashed?',
+    nickname: ''
+  }
+
   res.status(200)
   // 정상 작동 여부 전송
   res.send('respond with a resource');
 });
 
 router.post('/logout', function(req, res, next) {
+  var input = {
+    type: 'logout',
+    clientid: 1234,
+
+    sessid: '16진수'
+    // 세션 ID 저장용 테이블 생성 필요
+  }
+
   // 자동 로그인 토큰 삭제 필요
   res.status(200)
   // 1
@@ -31,28 +61,61 @@ router.post('/logout', function(req, res, next) {
 
 /* info modifier. */
 router.post('/get/:data', function(req, res, next) {
+  var input = {
+    type: 'get',
+    data: 'usname/pfimg',
+    clientid: 1234,
+
+    sessid: '16진수'
+  }
+
   // username, profile img
   res.status(500)
   // 받아온 데이터
   res.send('respond with a resource');
 });
 
-/* create data. */
-router.post('/create/:data/', function(req, res, next) {
+/* create data. currently useless */
+/*router.post('/create/:data/', function(req, res, next) {
+  var input = {
+    type: 'create',
+    data: 'aulokey',
+    clientid: 1234,
+
+    sessid: '16진수'
+  }
+  // auth key는 sessid로 통합
   // auth key, auto-login key
   res.status(200)
   // 정상 작동 여부 전송
   res.send('respond with a resource');
-});
+});*/
 
 router.post('/verify/:data', function(req, res, next) {
-  // password, auth key, auto-login key
+  var input = {
+    type: 'verify',
+    data: 'password/aulokey/sessid',
+    clientid: 1234,
+
+    sessid: '16진수', // sessid 제외
+    value: '16진수',
+    sysinfo: '', // sessid 한정
+  }
+  // password, auto-login key, sessid
   res.status(200)
   // 0, 1
   res.send('respond with a resource');
 });
 
 router.post('/modify/:data', function(req, res, next) {
+  var input = {
+    type: 'create',
+    data: 'aulokey',
+    clientid: 1234,
+
+    sessid: '16진수'
+  }
+  
   // password, nickname
   res.status(200)
   res.send('respond with a resource');
