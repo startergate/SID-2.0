c6f17c41af2b927eddee61ddb4b856e1752f4d80
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-03-20 20:50:08 +0900

    Add register
    Now receive actual password

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
+/*jshint esversion: 9 */
+
 const express = require('express');
 const bodyparser = require('body-parser');
 const mysql = require('mysql');
 const passport = require('passport');
+const sha256 = require('js-sha256').sha256;
 //const async = require('async');
 const router = express.Router();
 
@@ -10,7 +13,7 @@ var db_conn = mysql.createConnection({
   user: 'root',
   password: 'Wb4H9nn542',
   database: 'sid_userdata'
-})
+});
 
 db_conn.connect();
 
@@ -20,16 +23,16 @@ router.all('/', (req, res, next) => {
 
     is_vaild: false,
     description: 'Request to ROOT directory of api is prohibited'
-  }
+  };
   res.send(output);
 });
 
 /* login related functions. */
-router.post('/login', async (req, res, next, lc = new loginContainer()) => {
+router.post('/login', async (req, res, next) => {
 
   // POST DATA 무결성 검증
   if (!(req.body.type === 'login' && jsonChecker(req.body, ['clientid', 'userid', 'password'], [true, true, true]))) {
-    res.status(400)
+    res.status(400);
     res.send({
       type: 'error',
 
@@ -40,9 +43,9 @@ router.post('/login', async (req, res, next, lc = new loginContainer()) => {
   }
 
   // TODO: SQL 인젝션 대비 필요
-  var id = req.body.userid // receive POST json ID
-  var pw = req.body.password // receive POST json hashed PW
-  var clientid = req.body.clientid // receive POST json CID
+  var id = req.body.userid; // receive POST json ID
+  var pw = req.body.password; // receive POST json hashed PW
+  var clientid = req.body.clientid; // receive POST json CID
 
   await db_conn.query('SELECT client_data FROM client_list WHERE (clientid LIKE \'' + req.body.clientid + '\')', async (error, results, fields) => {
     if (error) throw error;
@@ -56,6 +59,8 @@ router.post('/login', async (req, res, next, lc = new loginContainer()) => {
       });
       return;
     }
+
+    pw = sha256(pw);
     db_conn.query('SELECT pid FROM userdata WHERE (id LIKE \'' + id + '\') AND (pw LIKE \'' + pw + '\')', async (error, results, fields) => {
       if (error) throw error;
       if (results.length < 1) {
@@ -69,16 +74,16 @@ router.post('/login', async (req, res, next, lc = new loginContainer()) => {
         return;
       }
       var expireData = new Date();
-      expireData.setUTCMonth((expireData.getUTCMonth() + 3) % 11)
+      expireData.setUTCMonth((expireData.getUTCMonth() + 3) % 11);
       if ((expireData.getUTCMonth() + 3) / 11 > 1) {
-        expireData.setUTCFullYear(expireData.getUTCFullYear() + 1)
+        expireData.setUTCFullYear(expireData.getUTCFullYear() + 1);
       }
       expireData = expireData.toISOString().slice(0, 19).replace('T', ' ');
-      sessid = randomString(64)
+      sessid = randomString(64);
 
       db_conn.query(`INSERT INTO session_list (sessid, pid, clientid, expire) VALUES ('${sessid}', '${results[0].pid}', '${clientid}', '${expireData}')`, (error, results, fields) => {
         if (error) throw error;
-      })
+      });
 
       await res.status(200);
       await res.send({
@@ -101,19 +106,19 @@ router.post('/login', async (req, res, next, lc = new loginContainer()) => {
   });
 });
 
-router.post('/register', (req, res, next) => {
+router.post('/register', async (req, res, next) => {
   var input = {
     type: 'register',
     clientid: 1234,
 
     userid: 'userid',
-    password: 'hashed?',
+    password: 'non-hashed',
     nickname: ''
-  }
+  };
 
   // POST DATA 무결성 검증
   if (!(req.body.type === 'register' && jsonChecker(req.body, ['clientid', 'userid', 'password'], [true, true, true]))) {
-    res.status(400)
+    res.status(400);
     res.send({
       type: 'error',
 
@@ -123,17 +128,52 @@ router.post('/register', (req, res, next) => {
     return;
   }
 
-  db_conn.query(`INSERT INTO userdata ()`)
-
-  res.status(200)
-  // 정상 작동 여부 전송
-  var output = {
-    type: 'response',
-
-    is_vaild: true,
-    is_succeed: true
+  var id = req.body.userid; // receive POST json ID
+  var pw = req.body.password; // receive POST json hashed PW
+  var clientid = req.body.clientid; // receive POST json CID
+  var nickname = req.body.nickname; // receive POST json CID
+  if (nickname == '') {
+    nickname = id;
   }
-  res.send('respond with a resource');
+
+  await db_conn.query('SELECT client_data FROM client_list WHERE (clientid LIKE \'' + req.body.clientid + '\')', async (error, results, fields) => {
+    if (error) throw error;
+    if (results.length < 1) {
+      await res.status(400);
+      await res.send({
+        type: 'error',
+
+        is_vaild: false,
+        error: 'Error with Client ID'
+      });
+      return;
+    }
+    pw = sha256(pw);
+    sessid = randomString(32);
+    db_conn.query(`INSERT INTO userdata (id,pw,nickname,register_date,pid) VALUES('${id}', '${pw}', '${nickname}', now(), '${sessid}')`, async (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        res.status(500);
+        // 정상 작동 여부 전송
+        res.send({
+          type: 'response',
+
+          is_vaild: true,
+          is_succeed: false
+        });
+        return;
+      }
+
+      res.status(200);
+      // 정상 작동 여부 전송
+      res.send({
+        type: 'response',
+
+        is_vaild: true,
+        is_succeed: true
+      });
+    });
+  });
 });
 
 router.post('/logout', function(req, res, next) {
@@ -142,7 +182,7 @@ router.post('/logout', function(req, res, next) {
     clientid: 1234,
 
     sessid: '16진수'
-  }
+  };
   // 세션 ID 저장용 테이블 생성 필요
   var output = {
     type: 'response',
@@ -150,9 +190,9 @@ router.post('/logout', function(req, res, next) {
 
     is_vaild: true,
     is_succeed: true
-  }
+  };
   // 자동 로그인 토큰 삭제 필요
-  res.status(200)
+  res.status(200);
   // 1
   res.send('respond with a resource');
 });
@@ -165,10 +205,10 @@ router.post('/get/:data', function(req, res, next) {
     clientid: 1234,
 
     sessid: '16진수'
-  }
+  };
 
   // username, profile img
-  res.status(200)
+  res.status(200);
   // 받아온 데이터
   var output = {
     type: 'response',
@@ -177,7 +217,7 @@ router.post('/get/:data', function(req, res, next) {
     is_vaild: true,
     requested_data: 'usname/pfimg',
     response_data: 'string'
-  }
+  };
   res.send('respond with a resource');
 });
 
@@ -203,7 +243,7 @@ router.post('/create/:data/', function(req, res, next) {
 
   }
   // auto-login key
-  res.status(404)
+  res.status(404);
   // 정상 작동 여부 전송
   res.send({
     message: 'use vaild data type'
@@ -217,18 +257,17 @@ router.post('/verify/:data', function(req, res, next) {
     clientid: 1234,
 
     sessid: '16진수', // sessid 제외
-    value: '16진수',
-    sysinfo: '' // sessid 한정
-  }
+    value: '16진수'
+  };
   // password, auto-login key, sessid
-  res.status(200)
+  res.status(200);
   // 0, 1
   var output = {
     type: 'response',
 
 
     is_vaild: true
-  }
+  };
   res.send('respond with a resource');
 });
 
@@ -239,10 +278,10 @@ router.post('/modify/:data', function(req, res, next) {
     clientid: 1234,
 
     sessid: '16진수'
-  }
+  };
 
   // password, nickname
-  res.status(200)
+  res.status(200);
   var output = {
     type: 'response',
 
@@ -251,33 +290,35 @@ router.post('/modify/:data', function(req, res, next) {
     is_processed: true,
 
     original_data: '1234'
-  }
+  };
   res.send('respond with a resource');
 });
 
 var checkExist = function(targetDB, targetName, targetValue, valueType = 'string') {
   try {
-    connection.connect()
+    connection.connect();
+    var sql;
     if (valueType === 'string') {
-      var sql = "SELECT * FROM "+ targetDB + " WHERE " + targetName + " = '" + targetValue + "'";
+      sql = "SELECT * FROM " + targetDB + " WHERE " + targetName + " = '" + targetValue + "'";
     } else {
-      var sql = "SELECT * FROM "+ targetDB + " WHERE " + targetName + " = " + targetValue;
+      sql = "SELECT * FROM " + targetDB + " WHERE " + targetName + " = " + targetValue;
     }
   } catch (e) {
-    return -1
+    return -1;
   }
-}
+};
 
 var randomString = function(length) {
   var character = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var rendom_str = '';
   var loopNum = length;
-  while (loopNum-=1) {
-      rendom_str += character[Math.floor(Math.random() * character.length)];
+  while (loopNum) {
+    rendom_str += character[Math.floor(Math.random() * character.length)];
+    loopNum -= 1;
   }
 
   return rendom_str;
-}
+};
 
 var logCreater = (type, time, datatype) => {
   if (type == 'error') {
@@ -285,48 +326,26 @@ var logCreater = (type, time, datatype) => {
   } else {
     // response_log
   }
-}
+};
 
 var jsonChecker = (_json, variablesArray, isMustFilled) => {
   if (!Array.isArray(variablesArray) || !Array.isArray(isMustFilled)) {
     throw new TypeError('Input Data is not an array');
-    return;
   }
   if (typeof _json !== 'object') {
     throw new TypeError('Input Data is not an object');
-    return;
   }
-  var cnt = 0
+  var cnt = 0;
   for (var data in _json) {
     if (variablesArray.indexOf(data) === -1) continue;
     if (!(_json[data] || !isMustFilled[cnt])) return 0;
 
-    cnt++
+    cnt++;
   }
   if (cnt !== variablesArray.length) return 0;
   for (var variable in variablesArray) {
     if (!_json.hasOwnProperty(variablesArray[variable])) return 0;
   }
   return 1;
-}
-
-class loginContainer {
-  constructor() {
-    this.cid = 0;
-    this.pid = 0;
-  }
-  setCid(cid) {
-    this.cid = cid;
-  }
-  setPid(pid) {
-    this.pid = pid;
-  }
-  getCid() {
-    return cid;
-  }
-  getPid() {
-    return pid;
-  }
-}
-
+};
 module.exports = router;
