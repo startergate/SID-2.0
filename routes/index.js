c9ef9dce15e2386dd4551b0d3aed0afc1164c0bd
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-10 14:29:13 +0900

    여러가지

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+/*jshint esversion: 9 */
+
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
 
 module.exports = router;
